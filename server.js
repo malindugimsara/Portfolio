7c56c4cbc569dad360ec5f8650c7fd04ce1a98cd
Malindu <gimsaramalindu2002@gmail.com>
2026-01-13 16:47:33 +0530

    add .env file

--- server.js
+++ server.js
@@ -9,14 +9,12 @@ app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
 
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: "gimsaramalindu2002@gmail.com",
-    pass: "icffiyspkzwvamka"
+    pass: process.env.EMAIL_PASS
   },
 });
 
