ef09ea340822f3c4c9dd0bb48fe7568f5fd4ddaf
Malindu <gimsaramalindu2002@gmail.com>
2026-01-16 13:32:31 +0530

    change nav bar status

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,17 +15,30 @@ export const NavBar = () => {
 
   useEffect(() => {
     const onScroll = () => {
-      if (window.scrollY > 50) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
+      const scrollPos = window.scrollY + 100;
+
+      const home = document.getElementById("home");
+      const projects = document.getElementById("projects");
+      const education = document.getElementById("education");
+      const leadership = document.getElementById("leadership");
+
+      if (leadership && scrollPos >= leadership.offsetTop) {
+        setActiveLink("leadership");
+      } else if (education && scrollPos >= education.offsetTop) {
+        setActiveLink("education");
+      } else if (projects && scrollPos >= projects.offsetTop) {
+        setActiveLink("projects");
+      } else if (home && scrollPos >= home.offsetTop) {
+        setActiveLink("home");
       }
-    }
+
+      setScrolled(window.scrollY > 50);
+    };
 
     window.addEventListener("scroll", onScroll);
-
     return () => window.removeEventListener("scroll", onScroll);
-  }, [])
+  }, []);
+
 
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
